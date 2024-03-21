b1912e21a189ee350c1d2715adaaa380825351ce
Dione V. Silva <135567312+Dionepx@users.noreply.github.com>
2024-03-20 23:12:03 -0300

    Add files via upload

---
+++ shiftMethod/script.js
@@ -0,0 +1,12 @@
+/*
+  03
+ 
+  - Exiba no console o array abaixo com a ordem dos itens invertida;
+  - O resultado exibido deve ser [1, 2, 3];
+ 
+  Dica: procure pelo método reverse, no MDN.
+*/
+
+let randomNumbers = [3, 2, 1]
+randomNumbers.reverse()
+// console.log(randomNumbers)
